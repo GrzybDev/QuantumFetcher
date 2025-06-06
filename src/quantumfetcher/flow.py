fb028fedf0a8d5300a9569ea6e079e9ff4d336d0
Marek Grzyb <marek@grzyb.dev>
2025-06-06 23:15:19 +0200

    Finished proof-of-concept downloader

---
+++ src/quantumfetcher/flow.py
@@ -0,0 +1,139 @@
+from urllib.parse import urlparse, urlunparse
+
+import typer
+from rich.console import Group
+from rich.live import Live
+from rich.progress import (
+    BarColumn,
+    DownloadColumn,
+    MofNCompleteColumn,
+    Progress,
+    SpinnerColumn,
+    TaskProgressColumn,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+    TransferSpeedColumn,
+)
+
+from quantumfetcher.downloader import download_media
+from quantumfetcher.enumerators.StreamType import StreamType
+
+
+class DownloadFlow:
+
+    __progress_overall = Progress(
+        SpinnerColumn(finished_text="\u2713"),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TaskProgressColumn(),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+    )
+
+    __progress_stream = Progress(
+        SpinnerColumn(finished_text="\u2713"),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        MofNCompleteColumn(),
+        TaskProgressColumn(),
+        TimeElapsedColumn(),
+    )
+
+    __progress_media = Progress(
+        SpinnerColumn(finished_text="\u2713"),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        DownloadColumn(),
+        TransferSpeedColumn(),
+        TaskProgressColumn(),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+    )
+    __progress_group = Group(__progress_overall, __progress_stream, __progress_media)
+
+    def __init__(self, videoList, manifests, output_path) -> None:
+        self.__videoList = videoList
+        self.__manifests = manifests
+        self.__output_path = output_path
+
+        output_path.mkdir(exist_ok=True)
+
+    def download(self, fetch_episodes, fetch_qualities):
+        with Live(self.__progress_group):
+            progress_overall = self.__progress_overall.add_task(
+                "Downloading episodes...", total=len(fetch_episodes)
+            )
+
+            for episode in fetch_episodes:
+                self.__progress_overall.update(
+                    progress_overall, advance=0, description=f"Downloading {episode}..."
+                )
+
+                self.__download_episode(episode, fetch_qualities)
+
+                self.__progress_overall.update(progress_overall, advance=1)
+                break
+
+    def __get_streams_to_fetch(self, episode_id, qualities):
+        # Get only the streams that match the quality
+        server_manifest, client_manifest = self.__manifests[episode_id]
+        output = {StreamType.Video: [], StreamType.Audio: [], StreamType.Text: []}
+
+        for stream_type in qualities:
+            for ql in qualities[stream_type]:
+                match stream_type:
+                    case StreamType.Video:
+                        stream = server_manifest.get_video_stream(ql.bitrate)
+                        chunks = client_manifest.get_chunks_count(StreamType.Video)
+
+                        if stream:
+                            output[StreamType.Video].append((stream, chunks))
+                        else:
+                            typer.echo(
+                                f"Cannot find Video stream with requested quality for episode {episode_id}, skipping...",
+                                err=True,
+                            )
+                    case StreamType.Audio | StreamType.Text:
+                        stream = server_manifest.get_named_stream(
+                            ql.name if episode_id != "J1 - 4K Test" else "eng",
+                            stream_type,
+                            ql.bitrate,
+                        )
+                        chunks = client_manifest.get_chunks_count(stream_type)
+
+                        if stream:
+                            output[stream_type].append((stream, chunks))
+                        else:
+                            typer.echo(
+                                f"Cannot find {stream_type.name} stream ({ql.name}) with requested quality for episode {episode_id}, skipping...",
+                                err=True,
+                            )
+        return output
+
+    def __download_episode(self, episode_id, qualities):
+        episode_path = self.__output_path / episode_id
+        episode_path.mkdir(exist_ok=True)
+
+        media_to_download = self.__get_streams_to_fetch(episode_id, qualities)
+
+        for stream_type in media_to_download:
+            self.__download_streams(
+                episode_id, episode_path, stream_type, media_to_download[stream_type]
+            )
+
+    def __download_streams(self, episode_id, episode_path, type, streams):
+        progress_stream = self.__progress_stream.add_task(
+            f"Downloading {type.name} streams...", total=len(streams)
+        )
+
+        for stream, chunks in streams:
+            filename = stream.attributes.get("src")
+            media_url = self.__videoList.get_media_url(episode_id, filename)
+
+            download_media(
+                media_url, chunks, episode_path / filename, self.__progress_media
+            )
+
+            self.__progress_stream.update(progress_stream, advance=1)
