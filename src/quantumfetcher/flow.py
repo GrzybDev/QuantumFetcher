3eb83bf4e8e2a297c2faaf8ad4827c0e27952dc0
Marek Grzyb <marek@grzyb.dev>
2025-06-11 21:11:20 +0200

    Added subtitle extract functionality

--- src/quantumfetcher/flow.py
+++ src/quantumfetcher/flow.py
@@ -1,3 +1,4 @@
+import re
 from urllib.parse import urlparse, urlunparse
 
 import typer
@@ -18,6 +19,7 @@ from rich.progress import (
 
 from quantumfetcher.downloader import download_media
 from quantumfetcher.enumerators.StreamType import StreamType
+from quantumfetcher.subtitles import extract_subtitles
 
 
 class DownloadFlow:
@@ -51,30 +53,42 @@ class DownloadFlow:
         TimeElapsedColumn(),
         TimeRemainingColumn(),
     )
-    __progress_group = Group(__progress_overall, __progress_stream, __progress_media)
 
-    def __init__(self, videoList, manifests, output_path) -> None:
+    __progress_subtitle = Progress(
+        SpinnerColumn(finished_text="\u2713"),
+        TextColumn("[progress.description]{task.description}"),
+    )
+
+    __progress_group = Group(
+        __progress_overall, __progress_stream, __progress_media, __progress_subtitle
+    )
+
+    def __init__(self, videoList, manifests, output_path, extract_subtitles) -> None:
         self.__videoList = videoList
         self.__manifests = manifests
         self.__output_path = output_path
+        self.__extract_subtitles = extract_subtitles
 
         output_path.mkdir(exist_ok=True)
 
     def download(self, fetch_episodes, fetch_qualities):
-        with Live(self.__progress_group):
+        with Live(self.__progress_group, refresh_per_second=10):
             progress_overall = self.__progress_overall.add_task(
                 "Downloading episodes...", total=len(fetch_episodes)
             )
 
             for episode in fetch_episodes:
                 self.__progress_overall.update(
-                    progress_overall, advance=0, description=f"Downloading {episode}..."
+                    progress_overall, description=f"Downloading {episode}..."
                 )
 
                 self.__download_episode(episode, fetch_qualities)
 
                 self.__progress_overall.update(progress_overall, advance=1)
-                break
+
+            self.__progress_overall.update(
+                progress_overall, completed=True, visible=False
+            )
 
     def __get_streams_to_fetch(self, episode_id, qualities):
         # Get only the streams that match the quality
@@ -118,16 +132,29 @@ class DownloadFlow:
 
         media_to_download = self.__get_streams_to_fetch(episode_id, qualities)
 
-        for stream_type in media_to_download:
-            self.__download_streams(
-                episode_id, episode_path, stream_type, media_to_download[stream_type]
-            )
-
-    def __download_streams(self, episode_id, episode_path, type, streams):
         progress_stream = self.__progress_stream.add_task(
-            f"Downloading {type.name} streams...", total=len(streams)
+            "Downloading episode media...", total=len(media_to_download)
         )
 
+        for stream_type in media_to_download:
+            self.__progress_stream.update(
+                progress_stream,
+                description=f"Downloading {stream_type.name} Streams...",
+            )
+
+            self.__download_streams(
+                episode_id,
+                episode_path,
+                media_to_download[stream_type],
+                progress_stream,
+                stream_type == StreamType.Text and self.__extract_subtitles,
+            )
+
+            self.__progress_stream.advance(progress_stream)
+
+        self.__progress_stream.update(progress_stream, completed=True, visible=False)
+
+    def __download_streams(self, episode_id, episode_path, streams, progress, extract):
         for stream, chunks in streams:
             filename = stream.attributes.get("src")
             media_url = self.__videoList.get_media_url(episode_id, filename)
@@ -136,4 +163,27 @@ class DownloadFlow:
                 media_url, chunks, episode_path / filename, self.__progress_media
             )
 
-            self.__progress_stream.update(progress_stream, advance=1)
+            if extract:
+                subtitle_task = self.__progress_subtitle.add_task(
+                    "Extracting captions...", total=1
+                )
+                match = re.match(r"J(\d).*", episode_id)
+                episode_id = -1
+
+                if match:
+                    episode_id = match.group(1)
+
+                extract_subtitles(
+                    episode_path / filename,
+                    episode_num=int(episode_id),
+                    track_name=stream.parameters.get("trackName", "unknown"),
+                )
+
+                self.__progress_subtitle.update(
+                    subtitle_task, advance=1, completed=True, visible=False
+                )
+
+            self.__progress_stream.update(progress, advance=1)
+
+        for task in self.__progress_media.tasks:
+            self.__progress_media.update(task.id, completed=True, visible=False)
