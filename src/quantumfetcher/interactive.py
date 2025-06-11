1c33e65e2e86c6f90246577ed781b76ab25d0806
Marek Grzyb <marek@grzyb.dev>
2025-06-11 21:11:20 +0200

    Added subtitle extract functionality

--- src/quantumfetcher/interactive.py
+++ src/quantumfetcher/interactive.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import typer
+
 from quantumfetcher import surveys
+from quantumfetcher.enumerators.StreamType import StreamType
 from quantumfetcher.flow import DownloadFlow
 from quantumfetcher.videolist import VideoList
 
@@ -10,9 +13,18 @@ def InteractiveMain(gamePath: Path, videoList_path: Path, episodes_path: Path):
 
     episodes_to_fetch, manifests = surveys.get_episodes(videoList)
     qualities_to_fetch = surveys.get_streams(episodes_to_fetch, manifests)
+    extract_subtitles = False
+
+    if qualities_to_fetch[StreamType.Text]:
+        extract_subtitles = typer.confirm(
+            "Do you want to extract subtitles to editable format?"
+        )
 
     flow = DownloadFlow(
-        videoList=videoList, manifests=manifests, output_path=gamePath / episodes_path
+        videoList=videoList,
+        manifests=manifests,
+        output_path=gamePath / episodes_path,
+        extract_subtitles=extract_subtitles,
     )
 
     flow.download(episodes_to_fetch, qualities_to_fetch)
