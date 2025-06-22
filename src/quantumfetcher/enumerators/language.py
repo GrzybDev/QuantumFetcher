b0d0ef21fc98a6c8591a34ccf127e84bcfff6f8a
Marek Grzyb <marek@grzyb.dev>
2025-06-22 14:45:32 +0200

    Added language enumerator

---
+++ src/quantumfetcher/enumerators/language.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Language(Enum):
+    Unknown = "unk"
+    English = "eng"
+    French = "fra"
+    Portuguese = "por"
+    Russian = "rus"
+    Spanish = "spa"
+    German = "deu"
+    Italian = "ita"
+    Chinese = "zho"
+    Japanese = "jpn"
+    Korean = "kor"
